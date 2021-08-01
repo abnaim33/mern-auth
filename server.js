2c42204d2a0f4d9d24ba0e0aa3856fd8822722be
abnaim33 <67532457+abnaim33@users.noreply.github.com>
2021-08-01 08:53:07 +0600

    first commit

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const config = require('config')
-
+require('dotenv').config()
 const app = express()
 
 // Passing middleware
 app.use(express.json())
 
-const db = config.get('mongoURI')
+const db = process.env.MONGO_URL
 
 mongoose
     .connect(db, {
-        useNewUrlParser : true,
-        useCreateIndex : true,
-        useUnifiedTopology : true
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true
     })
     .then(() => console.log('Database is connected...'))
-    .catch(err => console.log('Database connection error : '+err))
+    .catch(err => console.log('Database connection error : ' + err))
 
 // Routes
 app.use('/api/users', require('./routes/api/user'))
@@ -24,5 +24,5 @@ app.use('/api/auth', require('./routes/api/auth'))
 
 const Port = process.env.PORT || 5000;
 app.listen(Port, () => {
-    console.log(`Server is running at Port `+ Port )
+    console.log(`Server is running at Port ` + Port)
 })
