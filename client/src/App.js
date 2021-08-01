0304f24f057ba60c62abc70d2783a34234a43422
abnaim33 <67532457+abnaim33@users.noreply.github.com>
2021-08-01 09:04:43 +0600

    second commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import AuthContextProvider from './contexts/AuthContext'
 import Routes from './Routes';
 
 class App extends Component {
-  state = {  }
-  render() { 
-    return ( 
-        <AuthContextProvider>
-          <Routes/>
-        </AuthContextProvider>
+  state = {}
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+
+
+          <AuthContextProvider>
+            <Route path="/">
+              <Routes />
+            </Route>
+
+          </AuthContextProvider>
+
+        </Switch>
+      </BrowserRouter>
+
     );
   }
 }
- 
+
 export default App;
